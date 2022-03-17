028a66e0da9b556f2940022d7789f2057b6db35c
Jacob Gade <jacobcgade@gmail.com>
2022-03-17 12:28:01 +0100

    Subject and Observer is now obsolete since we use events.

--- Ladeskab/Ladeskab_Classes/RFIDSimulator.cs
+++ Ladeskab/Ladeskab_Classes/RFIDSimulator.cs
@@ -3,24 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Observer;
 using Subject;
 
 namespace Ladeskab
 {
-    public class RFIDSimulator : Subject, IRFID
+    public class RFIDSimulator : IRFID
     {
         public event EventHandler<RFIDEventArgs>? RFIDStateEvent;
         public void RFIDDetected(int id)
         {
             RFIDEventArgs e = new RFIDEventArgs() { RFIDState = id };
             RFIDStateEvent?.Invoke(this, e);
-        }  
-
-        // Don't think this function is needed anymore, since the event does the job
-        public override void Notify()
-        {
-            Console.WriteLine("Notify called on RFIDSimulator");
         }
     }
 }
