f92a08eca97f61f386bcf25cbaf3347fbecbb086
Jacob Gade <jacobcgade@gmail.com>
2022-03-17 12:33:59 +0100

    Test that print output is correct

--- Ladeskab/LadeSkab_Classes.Unit.Test/TestDisplaySimulator.cs
+++ Ladeskab/LadeSkab_Classes.Unit.Test/TestDisplaySimulator.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// May be integration???
+// Arrange
+tempSensor.WillReturnTemp = 25;
+
+var stringWriter = new StringWriter();
+Console.SetOut(stringWriter);
+
+// Act
+uut.Regulate();
+
+// Assert
+var output = stringWriter.ToString();
+Assert.That(output, Is.EqualTo("Temperature measured was 25\r\n"));
