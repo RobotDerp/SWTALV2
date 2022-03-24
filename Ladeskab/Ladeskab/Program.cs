d48d249b45f5d4588429edaf12ffe6f09d24233a
Casper Stationær <samblebenc@gmail.com>
2022-03-24 11:23:28 +0100

    Main

--- Ladeskab/Ladeskab/Program.cs
+++ Ladeskab/Ladeskab/Program.cs
@@ -1,45 +1,56 @@
-﻿    class Program
+﻿using Ladeskab;
+using Ladeskab_Classes;
+
+class Program
     {
+     
+
         static void Main(string[] args)
         {
-            // Assemble your system here from all the classes
+        DoorSimulator door = new DoorSimulator();
+        RFIDSimulator rfidReader = new RFIDSimulator();
+        DisplaySimulator displaySimulator = new DisplaySimulator();
+        UsbChargerSimulator usb = new UsbChargerSimulator();
+        ChargeControl chargeControl = new ChargeControl(usb, displaySimulator);
+        StationControl stationControl = new StationControl(door, displaySimulator, usb, rfidReader);
+        //Assemble your system here from all the classes
 
-            //bool finish = false;
-            //do
-            //{
-            //    string input;
-            //    System.Console.WriteLine("Indtast E, O, C, R: ");
-            //    input = Console.ReadLine();
-            //    if (string.IsNullOrEmpty(input)) continue;
+        bool finish = false;
+        do
+        {
+            string input;
+            System.Console.WriteLine("Indtast E, O, C, R: ");
+            input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) continue;
 
-            //    switch (input[0])
-            //    {
-            //        case 'E':
-            //            finish = true;
-            //            break;
+            switch (input[0])
+            {
+                case 'E':
+                    finish = true;
+                    break;
 
-            //        case 'O':
-            //            door.OnDoorOpen();
-            //            break;
+                case 'O':
+                    door.OnDoorOpen();
+                    break;
 
-            //        case 'C':
-            //            door.OnDoorClose(); // door objekt skal tilføjes
-            //            break;
+                case 'C':
+                    door.OnDoorClose(); // door objekt skal tilføjes
+                    break;
 
-            //        case 'R':
-            //            System.Console.WriteLine("Indtast RFID id: ");
-            //            string idString = System.Console.ReadLine();
+                case 'R':
+                    System.Console.WriteLine("Indtast RFID id: ");
+                    string idString = System.Console.ReadLine();
 
-            //            int id = Convert.ToInt32(idString);
-            //            rfidReader.OnRfidRead(id);
-            //            break;
+                    int id = Convert.ToInt32(idString);
+                    rfidReader.RFIDDetected(id);
+                    break;
 
-            //        default:
-            //            break;
-            //    }
+                default:
+                    break;
+            }
 
-            //} while (!finish);
-            while (true)
+        } while (!finish);
+        while (true)
             { }
         }
     }
